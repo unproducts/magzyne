65fd35f0386d4e0b94b20412394f2a06cb389843
amandesai01 <amandesai01@gmail.com>
2023-08-08 00:55:09 +0530

    playground: dummy data

--- packages/web-layer/.playground/app.config.ts
+++ packages/web-layer/.playground/app.config.ts
@@ -1,5 +1,11 @@
 export default defineAppConfig({
   site: {
     name: 'Tech Laundry',
+  },
+  dataSource: {
+    ghAdminUsername: "amandesai01",
+    ghRepoUsername: "amandesai01",
+    ghRepoName: "amandesai01.github.io",
+    ghDsIssueNumber: 1,
   }
 })
