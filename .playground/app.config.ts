afdc513507e949b53fa4ae0d2839042a901113f1
amandesai01 <amandesai01@gmail.com>
2023-08-08 17:29:46 +0530

    playground: scaffold

---
+++ .playground/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  site: {
+    name: 'Tech Laundry 2',
+  },
+  dataSource: {
+    ghAdminUsername: "amandesai01",
+    ghRepoUsername: "amandesai01",
+    ghRepoName: "amandesai01.github.io",
+    ghDsIssueNumber: 1,
+  }
+})
