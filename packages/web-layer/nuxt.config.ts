610c814ca47d562633558f5e87f9d80769f27e56
amandesai01 <amandesai01@gmail.com>
2023-08-10 15:48:01 +0530

    layer:config migrate

--- packages/web-layer/nuxt.config.ts
+++ packages/web-layer/nuxt.config.ts
@@ -2,6 +2,7 @@
 
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
+import daisyui from "daisyui";
 
 const currentPath = dirname(fileURLToPath(import.meta.url));
 
@@ -9,7 +10,24 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   // @ts-ignore
   tailwindcss: {
-    configPath: join(currentPath, 'tailwind.config.js'),
+    config: {
+      content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+      ],
+      theme: {
+        extend: {},
+      },
+      daisyui: {
+        themes: ["black"], // only one theme
+      },
+    
+      plugins: [daisyui],
+    },
     cssPath: join(currentPath, 'main.css'),
   }
 });
