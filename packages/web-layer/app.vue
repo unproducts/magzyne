af9e7f0bdb27c8d06dd1ef67fbdcbbcbd2430b22
amandesai01 <amandesai01@gmail.com>
2023-08-15 23:59:06 +0530

    layer: project minified and deps fixed

--- packages/web-layer/app.vue
+++ packages/web-layer/app.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-// @ts-ignore [Vue] Property or method "useAppConfig" is not defined on the instance but referenced during render.
 const app = useAppConfig()
-// @ts-ignore [Vue] Property or method "useMagzyneDataFetcher" is not defined on the instance but referenced during render.
 const { newsData, newsDataError, newsDataLoading } = useMagzyneDataFetcher();
 </script>
 
 <template>
-  <div class="navbar bg-base-100 sticky top-0 z-50">
-    <a class="btn btn-ghost normal-case text-xl">{{ app.site.name }}</a>
-  </div>
-  <section class="flex flex-wrap gap-4 items-center justify-center" v-if="!newsDataLoading && !newsDataError">
-    <div class="lg:w-1/2">
-      <article :key="news.url" v-for="news in newsData">
+  <nav class="navbar">
+    <h1 class="navbar-title">{{ app.site.name }}</h1>
+  </nav>
+  <section class="article-grid" v-if="!newsDataLoading && !newsDataError">
+      <article :key="news.url" class="article" v-for="news in newsData">
         <a :href="news.url" target="_blank"
-          class="card md:card-side w-full bg-base-100 shadow-xl border border-primary hover:border-primary-focus m-2">
-          <div class="card-body p-4">
-            <h2 class="card-title">
-              {{ news.title }}
-            </h2>
-            <p class="max-h-1/3 h-1/3 overflow-y-hidden">{{ news.description?.slice(0, 200) + "..." }}</p>
-            <div class="badge badge-secondary text-sm">{{ news.source || "Unknown" }}</div>
-          </div>
-          <figure class="w-3/4" v-if="news.image != '-'"><img :src="news.image" :alt="'Cover image of' + news.title" />
-          </figure>
+          class="">
+          <img :src="news.image" :alt="'Cover image of' + news.title" v-if="news.image != '-'"/>
+          <h2>{{ news.title }}</h2>
+          <p>{{ news.description?.slice(0, 200) + "..." }}</p>
+          <p class="article-source">{{ news.source }}</p>
         </a>
       </article>
-    </div>
   </section>
 </template>
--- packages/web-layer/nuxt.config.ts
+++ packages/web-layer/nuxt.config.ts
@@ -2,32 +2,9 @@
 
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
-import daisyui from "daisyui";
 
 const currentPath = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  // @ts-ignore
-  tailwindcss: {
-    config: {
-      content: [
-        "./components/**/*.{js,vue,ts}",
-        "./layouts/**/*.vue",
-        "./pages/**/*.vue",
-        "./plugins/**/*.{js,ts}",
-        "./nuxt.config.{js,ts}",
-        "./app.vue",
-      ],
-      theme: {
-        extend: {},
-      },
-      daisyui: {
-        themes: ["black"], // only one theme
-      },
-    
-      plugins: [daisyui],
-    },
-    cssPath: join(currentPath, 'main.css'),
-  }
+  css: [join(currentPath, "main.css")],
 });
